b6154c7fe61fbf20268b966d3d2ff767f5df0795
5935512009 <Pasut2541@hotmail.com>
2020-09-19 00:02:03 -0500

    second commit

--- src/views/weare.vue
+++ src/views/weare.vue
@@ -3,33 +3,9 @@
       <h1>Demo1</h1>
       <div>this page  in weare.vue !</div>
       <div class="text-white text-3xl">I think this page will be the information about lifelinemeet</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
+      
 
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div><div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white ">test full screen</div>
-      <div class="text-white text-4xl">weare.vue</div>
+      
   </div>
 </template>
 
