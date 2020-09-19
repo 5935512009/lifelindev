27f07f6925eb25eccec96a7db23298c4bddf719e
5935512009 <Pasut2541@hotmail.com>
2020-09-19 03:25:42 -0500

    test contact

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,14 +1,6 @@
 <template>
   <div id="about1" class="about bg-white bg-opacity-50 w-2/4 absolute">
-    <form class="contact-form" @submit.prevent="sendEmail">
-    <label>Name</label>
-    <input type="text" name="user_name">
-    <label>Email</label>
-    <input type="email" name="user_email">
-    <label>Message</label>
-    <textarea name="message"></textarea>
-    <input type="submit" value="Send">
-  </form>
+    
     
    
   </div>
