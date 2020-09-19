3cc58cf1524de598f81b0d4601bcd5768652885f
5935512009 <Pasut2541@hotmail.com>
2020-09-19 04:16:11 -0500

    5935512009

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="about1" class="about bg-white bg-opacity-50 w-2/4 absolute">
+  <div id="about2" class="about bg-white bg-opacity-50 w-2/4 absolute">
     <div>   <div class="text-2xl underline">lifelineproject</div>มีวัตถุประสงค์เพื่อที่จะใช้ผู้ใช้สามารถทำการจัดการประชุม หรือการนัดพบต่างๆ 
       ได้อย่างสะดวงรวดเร็ว โดยการเปรียบเทียบช่วงเวลาว่างของเเต่ละคน เเล้วหาช่วงเวลาที่เหมาะสมที่สุดในการ meeting</div>
     <div>เตือนคำสั่ง(รูปแบบการทำงาน)</div>
@@ -11,9 +11,9 @@
   </div>
 </template>
 <style>
-#about1{
-
+#about2{
+  
   top: 30%;
-  left: 30%;
+  left: 28%;
 }
 </style>
--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,13 +1,20 @@
 <template>
-  <div id="about1" class="about bg-white bg-opacity-50 w-2/4 absolute">
+  <div id="about1" class="bg-white bg-opacity-50  absolute ">
+
+    <div class="text-3xl">Contact Us</div>
     <form class="contact-form" @submit.prevent="sendEmail">
-    <label>Name</label>
-    <input type="text" name="user_name">
-    <label>Email</label>
-    <input type="email" name="user_email">
-    <label>Message</label>
+    
+    <div class=" space-y-6 space-x-2"> <label>Name</label>
+    <input type="text" name="user_name"></div>
+
+    <div class="space-y-6 space-x-2"><label>Email</label>
+    <input type="email" name="user_email"></div>
+
+    <div class="space-y-6 space-x-2"> <label>Message</label>
     <textarea name="message"></textarea>
-    <input type="submit" value="Send">
+    <div class="text-center ">
+    <input type="submit" value="Send"></div></div>
+
   </form>
     
    
@@ -15,5 +22,14 @@
   
 </template>
 <style >
-    
+#about1{
+  height:auto ;
+  width: 400px;
+  text-align: center;
+  top: 30%;
+  right: 37%;
+  
+
+}
+
 </style>
