9f64f1b3d69cf0f8fe942b8f4d72f8070078cb04
5935512009 <Pasut2541@hotmail.com>
2021-02-05 08:07:24 -0500

    import(login.vue): add register

--- src/views/loginpage.vue
+++ src/views/loginpage.vue
@@ -4,7 +4,7 @@
     <h1>{{ msg }}</h1>
     
     <div id="goo1" class="text-white bg-white bg-opacity-25 absolute text-center">
-      <form @submit="dd">
+      <form @submit.prevent="pressed">
       <div class="space-y-4 space-x-6 pl-5 text-black">
         <label class="text-white">Email</label>
         <input type="email" name="user_email" placeholder="email" v-model="email">
@@ -16,9 +16,11 @@
         <input v-model="password" type="password" class="input" placeholder="Password">
       </div>
       <button>login</button>
+      
     </form>
+    <button><router-link to="/register">Register</router-link></button>
     <div class="pb-5">................................................................</div>
-
+    
     <button @click="socialGoogleLogin">Google</button></div>
     
   </div>
@@ -44,18 +46,32 @@ export default {
             fullname: result.additionalUserInfo.profile.name,
             email: result.additionalUserInfo.profile.email,
             profile_image: result.additionalUserInfo.profile.picture,
-            user_type_id: 1
+            user_type_id: 1,
+            uid: result.user.uid 
           };
           console.log(obj)
-          this.$router.push({ name: 'weare' });
+          this.$router.push({ name: 'menu_Scheduler' });
         })
         .catch(err => {
           alert("Oops. " + err.message);
         });
     },
     handleSumbit: function() {
-
+    },
+    pressed() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(data => {
+          console.log(data);
+          this.$router.replace({ name: "weare" });
+        })
+        .catch(error => {
+          this.error = error;
+        });
+        
     }
+    
   }
 };
 
@@ -77,7 +93,7 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: white;
 }
 button{
   color:white;
