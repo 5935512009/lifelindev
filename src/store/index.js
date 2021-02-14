7c706812b4bc65d54b342325c05bccdc1600e2c6
5935512009 <Pasut2541@hotmail.com>
2021-02-13 23:49:20 -0500

    add(index.js):use Vues for UserID

--- src/store/index.js
+++ src/store/index.js
@@ -2,20 +2,55 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
-
-
-const store = new Vuex.Store({
-  state: {
-    count: 0
+//ทดลอง 03
+export default new Vuex.Store({
+  state:{
+    count:0,
+    userData: {}
   },
-  mutations: {
-    increment (state) {
-      state.count++
+  mutations:{
+    add(state,value){ // สร้าง function ชื่อ add รับ state กับ value 
+      state.count = value
+      // state.count++ // กรณีไม่ใส่ value เข้า
+    },
+    setloginData(state, value){
+      state.userData =  value
     }
-  }
+
+  },
+  getters:{
+    getCount(state){ // state ส่งข้อมูลมาให้ อัตโนมัติ
+      return state.count
+    },
+    getUserData(state){
+      return state.userData
+    }
+  },
+  actions:{ 
+    addAction(context){  // ต้องอ้าง context 
+      context.commit('add', this.getters.getCount+1)
+    },
+    delAction(context){
+      context.commit('add', this.state.count-1)
+    },
+    getloginData(context, value){  // ต้องอ้าง context 
+      context.commit('setloginData', value)
+    }, 
+  },
 })
-store.commit('increment')
+
+//(ปัจจับัน 2)
+// const store = new Vuex.Store({
+//   state: {
+//     count: 0
+//   },
+//   mutations: {
+//     increment (state) {
+//       state.count++
+//     }
+//   }
+// })
+// store.commit('increment')
 
 // console.log('test', store.state.count)
 
