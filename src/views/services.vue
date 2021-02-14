0d43fe1cb1b4b30a053637568bb9023d25ed7b2e
5935512009 <Pasut2541@hotmail.com>
2021-02-13 23:54:58 -0500

    Test(service.vue):test ComA and Footer

--- src/views/services.vue
+++ src/views/services.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        
+    <div id="services" class="text-white text-3xl">
+        <h1>{{$store.getters.getCount}}</h1>
+        <hr>
+        <br>
+        <ComA/>
+        <hr>
+        <br>
+        <Footer title="this is the way Ltd" number="02" color="red"/>
+
     </div>
 </template>
+<script>
+import ComA from "../components/ComA"
+import Footer from "../components/Footer"
+export default {
+    name:"Services",
+    components:{
+        ComA,
+        Footer
+
+    },
+    
+}
+</script>
