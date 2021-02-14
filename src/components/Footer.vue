ffcb07a2b08e802b68e73ca99ad04be49ea68467
5935512009 <Pasut2541@hotmail.com>
2021-02-13 23:53:34 -0500

    New(Footer.vue):test props

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <span :style="{color}">{{title}}</span>
+      <span>{{number}}</span>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Footer",
+    props:[
+        "title",
+        "number",
+        "color",
+    ]
+}
+</script>
+
+<style scoped>
+.footer{
+    font-size:14+px;
+}
+</style>
