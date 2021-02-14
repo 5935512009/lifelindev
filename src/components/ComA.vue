4e3a46b39200c27bc363e31700313caa17e275d4
5935512009 <Pasut2541@hotmail.com>
2021-02-13 23:50:20 -0500

    New(ComA.vue):test vuex and props

---
+++ src/components/ComA.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="comA">
+      <h2>{{$store.getters.getCount}}</h2>
+      <button @click="add01">add</button>
+      <br>
+      <button @click="del02"> ลด</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ComA",
+    methods: {
+        add01(){
+            this.$store.dispatch("addAction")
+        },
+        del02(){
+            this.$store.dispatch("delAction")
+        }
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
